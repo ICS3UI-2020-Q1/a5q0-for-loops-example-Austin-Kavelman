6341e74e8977e11ec662411ccae04f6ad4214ff5
AustinKavelman <kavea9410@wrdsb.ca>
2020-10-06 13:00:37 +0000

    finished the for loop examples.

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * for loops examples
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +11,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //creat the count variable
+    int count = 1;
+    //create wile loop to count
+    while(count <= 10){
+    //print number to the concole
+    System.out.println(count);
+    //increase the counter
+    count = count + 1;
+    }
+
+    //do the same thing with a for loop
+    for(int count2 = 1; count2 <= 10; count2++){
+    //loop action print the number
+    System.out.println("Loop " + (count2+1));
+    }
+
+    //do the same thing with a for loop
+    for(int count2 = 1; count2 <= 10; count2++){
+    //loop action print the number
+    System.out.println("Loop " + (count2+1));
+    }
+
+
+
   }
 }
